68e96805f4a51ea2cfa3d1e8e8e4b2927d6c8804
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-05 18:46:35 +0300

    feat: middleware passing data

--- index.js
+++ index.js
@@ -1,18 +1,85 @@
 const express = require("express");
 require("dotenv").config();
+const logger = require("morgan");
 
-console.log(process.env);
+// console.log(process.env);
 const port = process.env.port || 5001;
 const host = "localhost";
 const app = express();
 
-
 // middleware
 
+app.use(logger());
 
+app.use((req, res, next) => {
+  //Authentication
+  console.log("Auth");
+  // some jobs
+  res.isAuth = false;
+  next();
+});
+app.use((req, res, next) => {
+  console.log("Controller // db");
+  if (res.isAuth) {
+    console.log("Private data");
+  } else {
+    console.log("not auth");
+  }
+  next();
+});
+app.use((req, res, next) => {
+  console.log("middleware: 3");
+  if (true) {
+    res.send("send");
+  }
+});
 
+app.get("/user", (req, res) => {
+  res.send("User Details");
+});
+
+app.use((err, req, res, next) => {
+  console.log("middleware: 1");
+  // yapılacak işlemler tanımlanır
+  const error = new err("BOOOOMMMMMM !!!");
+  next(error);
+});
+
+app.use((req, res, next) => {
+  console.log("middleware: 2");
+  // yapılacak işlemler tanımlanır
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("middleware: 3");
+  // yapılacak işlemler tanımlanır
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("middleware: 4");
+  // yapılacak işlemler tanımlanır
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("middleware: 5");
+  // yapılacak işlemler tanımlanır
+  if (true) {
+    res.send("OK");
+  }
+});
+
+app.use((req, res, next) => {
+  console.log("middleware: 6");
+  // yapılacak işlemler tanımlanır
+  next();
+});
+
+app.get("/user", (req, res) => {});
 
 const server = app.listen(port, "localhost", () => {
-  console.log(server.address());
+  // console.log(server.address());
   console.log("I'm listening on http://%s:%s", host, port);
 });
